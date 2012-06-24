bbfe5ca9f0eff855fc1404054bacf6b9946ea32d
Dean Morin <morin.dean@gmail.com>
2012-06-24 13:27:06 -0700

    forgot to add knocking script

---
+++ knock.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "$0 <ip>"
+    exit 1
+fi
+
+shared_secret="this will be used for the port knocking hash"
+random=$(cat /dev/urandom | tr -cd [:alnum:] | head -c 8)
+knock_port=25068
+port_to_open=00022
+prehash="$shared_secret$random$port_to_open"
+hashed=$(echo $prehash | sha1sum | awk '{print $1}')
+msg="$hashed$random$port_to_open"
+./client_util -e $msg
+
+len=$(expr length $msg)
+echo "Random: $random"
+echo "Hash: $hashed"
+
+#tcpid=$(./client_util -b $cmdlen)
+#srcprt=$(./client_util)
+#opts="-c 1 -2 -E /dev/stdin -d 100 -s $srcprt -p $knock_port -N $tcpid"
+opts="-c 1 -2 -d 100 -p $knock_port"
+
+hping3 $opts $1 --file temp_encrypted_message
+rm temp_encrypted_message
